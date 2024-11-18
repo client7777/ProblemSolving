d15e2887a9e2de4b895101a27c6c330cebfd4f22
client7777 <106692694+client7777@users.noreply.github.com>
2024-11-18 20:11:50 +0900

    ProblemSolving

---
+++ src/TwoPointer/boj_1806.java
@@ -0,0 +1,39 @@
+package TwoPointer;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class boj_1806
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        long[] arr = new long[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++)
+        {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        long min = Long.MAX_VALUE;
+        long tot = arr[0];
+        int end = 0;
+        for(int start =0; start<n; start++)
+        {
+            while (end < n && tot < s)
+            {
+                end++;
+                if(end != n) tot += arr[end];
+            }
+            if(end == n) break;
+            min = Math.min(min, end - start + 1);
+            tot -= arr[start];
+        }
+        System.out.print(min == Long.MAX_VALUE ? 0 : min);
+    }
+}
