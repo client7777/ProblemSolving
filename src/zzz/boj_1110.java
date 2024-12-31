40ff68c3ac28c7f7c643409873880c0de5ab1646
client7777 <106692694+client7777@users.noreply.github.com>
2024-12-31 17:44:56 +0900

    ProblemSolving

---
+++ src/zzz/boj_1110.java
@@ -0,0 +1,24 @@
+package zzz;
+
+import java.io.*;
+
+public class boj_1110
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int next = n;
+
+        int cnt = 0;
+        while (true)
+        {
+            cnt ++;
+
+            n = ((n % 10) * 10 + (n / 10 + n % 10) % 10);
+
+            if(n == next) break;;
+        }
+        System.out.print(cnt);
+    }
+}
