66ad5f627fc8d861a4bae90c4f477b056126aef8
client7777 <106692694+client7777@users.noreply.github.com>
2025-01-13 11:47:55 +0900

    Update 기능개발.java

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,14 +1,16 @@
 import java.util.*;
+
 class Solution 
 {
     public int[] solution(int[] progresses, int[] speeds) 
     {
-
         ArrayList<Integer> list = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
 
-        for (int i = 0; i < progresses.length; i++) {
-            if ((100 - progresses[i]) % speeds[i] == 0) {
+        for (int i = 0; i < progresses.length; i++) 
+        {
+            if ((100 - progresses[i]) % speeds[i] == 0) 
+            {
                 q.add((100 - progresses[i]) / speeds[i]);
             } else {
                 q.add((100 - progresses[i]) / speeds[i] + 1);
@@ -17,24 +19,27 @@ class Solution
 
         int x = q.poll();
         int count = 1;
-        while (!q.isEmpty()) {
-            if (x >= q.peek()) {
+        while (!q.isEmpty()) 
+        {
+            if (x >= q.peek()) 
+            {
                 count++;
                 q.poll();
-            } else {
+            } else 
+            {
                 list.add(count);
                 count = 1;
                 x = q.poll();
             }
         }
+        
         list.add(count);
 
         int[] answer = new int[list.size()];
-        for (int i = 0; i < answer.length; i++) {
+        for (int i = 0; i < answer.length; i++) 
+        {
             answer[i] = list.get(i);
         }
-
-
         return answer;
     }
 }
