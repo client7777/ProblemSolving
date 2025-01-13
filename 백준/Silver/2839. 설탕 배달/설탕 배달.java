e85dc6394cbb9e92450a423568a1881840e31ff0
client7777 <106692694+client7777@users.noreply.github.com>
2025-01-13 13:06:57 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14412 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        if(n < 3)
+        {
+            System.out.print(-1);
+            return;
+        }
+        
+        int[] d = new int[5001];
+
+        Arrays.fill(d, 5001);
+
+        if(n >= 3) d[3] = 1;
+        if(n >= 5) d[5] = 1;
+
+        for(int i=6; i<=n; i++)
+        {
+            if(d[i-3] != 5001 || d[i-5] != 5001)
+            {
+                d[i] = Math.min(d[i-3], d[i-5]) + 1;
+            }
+        }
+        System.out.print(d[n] == 5001 ? -1 : d[n]);
+    }
+}
