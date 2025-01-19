5ce6e1f1ede7c35be4961f77719a26cc2bb86a79
client7777 <106692694+client7777@users.noreply.github.com>
2025-01-19 13:52:50 +0900

    [Bronze II] Title: 문어 숫자, Time: 100 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/1864. 문어 숫자/문어 숫자.java
@@ -0,0 +1,65 @@
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        while (true)
+        {
+            String str = br.readLine();
+
+            if(str.equals("#")) break;
+
+            int length = str.length();
+
+            int num = 0;
+
+            for(int i=0; i<length; i++)
+            {
+                if(str.charAt(i) == '-')
+                {
+                    num += 0 * Math.pow(8, (length -1) -i);
+                }
+                else  if(str.charAt(i) == '\\')
+                {
+                    num += 1 * Math.pow(8, (length -1)-i);
+                }
+                else  if(str.charAt(i) == '(')
+                {
+                    num += 2 * Math.pow(8, (length -1)-i);
+                }
+                else  if(str.charAt(i) == '@')
+                {
+                    num += 3 * Math.pow(8, (length -1)-i);
+                }
+                else  if(str.charAt(i) == '?')
+                {
+                    num += 4 * Math.pow(8, (length -1)-i);
+                }
+                else  if(str.charAt(i) == '>')
+                {
+                    num += 5 * Math.pow(8, (length -1)-i);
+                }
+                else  if(str.charAt(i) == '&')
+                {
+                    num += 6 * Math.pow(8, (length -1)-i);
+                }
+                else  if(str.charAt(i) == '%')
+                {
+                    num += 7 * Math.pow(8, (length -1)-i);
+                }
+                else
+                {
+                    num += -1 * Math.pow(8, (length -1)-i);
+                }
+
+            }
+            sb.append(num).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
