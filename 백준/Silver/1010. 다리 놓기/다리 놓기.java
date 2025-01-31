f24ec4d46997c4164265ba491413d06eed861c0c
client7777 <106692694+client7777@users.noreply.github.com>
2025-01-31 23:14:35 +0900

    [Silver V] Title: 다리 놓기, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int test_case = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        int[][] d = new int[31][31];
+
+        for(int i=1; i<=30; i++)
+        {
+            d[i][1] = i; // n개 중에 1개를 뽑는 경우의 수
+            d[i][0] = 1;
+        }
+
+        for(int i=2; i<=30; i++)
+        {
+            for(int j=1; j<=30; j++)
+            {
+                d[i][j] = d[i-1][j-1] + d[i-1][j];
+            }
+        }
+        
+        for(int t=0; t<test_case; t++)
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            // M개중 N개를 뽑는 경우이므로 nCr 에서 n = M, r = N이다.
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            sb.append(d[m][n]).append('\n');
+
+        }
+        System.out.print(sb);
+    }
+}
