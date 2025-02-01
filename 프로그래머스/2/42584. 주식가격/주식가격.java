ee7af955dd7d70d15af137d73d1db0d2383dd51f
client7777 <106692694+client7777@users.noreply.github.com>
2025-02-01 22:33:13 +0900

    [level 2] Title: 주식가격, Time: 19.79 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution 
+{
+    public int[] solution(int[] prices)
+    {
+        int[] answer = new int[prices.length];
+
+        for(int i=0; i< prices.length; i++)
+        {
+            int curPrice = prices[i];
+            
+            int time = 0;
+            
+            for(int j=i+1; j<prices.length; j++)
+            {
+                if(curPrice <= prices[j]) time++;
+                else
+                {
+                    time++;
+                    break;
+                }
+            }
+            answer[i] = time;
+        }
+        
+        return answer;
+    }
+}
