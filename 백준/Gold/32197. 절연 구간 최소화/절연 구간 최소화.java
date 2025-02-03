c6767c051b470f78ffd4a91727d34bf664b2b514
client7777 <106692694+client7777@users.noreply.github.com>
2025-02-03 19:49:10 +0900

    [Gold IV] Title: 절연 구간 최소화, Time: 768 ms, Memory: 94876 KB -BaekjoonHub

---
+++ 백준/Gold/32197. 절연 구간 최소화/절연 구간 최소화.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main
+{
+    static int n,m,a,b;
+    static ArrayList<Edge>[] graph;
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n+1];
+        for(int i=1; i<=n; i++)
+        {
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<m; i++)
+        {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+
+            graph[u].add(new Edge(v,t,0));
+            graph[v].add(new Edge(u,t,0));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        dijkstra();
+    }
+
+    static void dijkstra()
+    {
+        int[][] cost = new int[n+1][2];
+        for(int i=1; i<=n; i++)
+        {
+            Arrays.fill(cost[i], Integer.MAX_VALUE);
+        }
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+
+        for(Edge startNode:graph[a])
+        {
+            pq.add(new Edge(a, startNode.state, 0));
+            cost[a][startNode.state] = 0;
+        }
+
+        while (!pq.isEmpty())
+        {
+            Edge cur = pq.poll();
+            int curNode = cur.node;
+            int curState = cur.state;
+            int curCost = cur.cost;
+
+            for(Edge next:graph[curNode])
+            {
+                int nextNode = next.node;
+                int nextState = next.state;
+                int nextCost = curCost + (curState == nextState ? 0 : 1);
+
+                if(cost[nextNode][nextState] > nextCost)
+                {
+                    cost[nextNode][nextState] = nextCost;
+                    pq.add(new Edge(nextNode, nextState, cost[nextNode][nextState]));
+                }
+            }
+        }
+        System.out.print(Math.min(cost[b][0], cost[b][1]));
+    }
+    static class Edge implements Comparable<Edge>
+    {
+        int node;
+        int state;
+        int cost;
+
+        public Edge(int node, int state, int cost) {
+            this.node = node;
+            this.state = state;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge o)
+        {
+            return Integer.compare(this.cost, o.cost);
+        }
+    }
+}
