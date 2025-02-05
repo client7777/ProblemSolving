4b4a9899f6ea4e5901ba8569e5895e5a07d56a86
client7777 <106692694+client7777@users.noreply.github.com>
2025-02-05 21:02:39 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num1, int num2) 
+    {
+        int ans = 0;
+        
+        if(num1 == num2)
+            ans = 1;
+        else ans = -1;
+        
+        return ans;
+    }
+}
