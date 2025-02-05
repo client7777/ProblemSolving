44e46244e4cfc1bb0a125f93b9caf47037285514
client7777 <106692694+client7777@users.noreply.github.com>
2025-02-05 22:10:06 +0900

    [Silver II] Title: 친구, Time: 108 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Silver/1058. 친구/친구.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] map = new int[n][n];
+
+        for(int i=0; i<n; i++)
+        {
+            String str = br.readLine();
+            for(int j=0; j<n; j++)
+            {
+                map[i][j] = str.charAt(j) == 'Y' ? 1 : Integer.MAX_VALUE / 2;
+            }
+        }
+
+        for(int k=0; k<n; k++)
+        {
+            for(int i=0; i<n; i++)
+            {
+                for(int j=0; j<n; j++)
+                {
+                    if(i == j) continue;
+                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+                }
+            }
+        }
+
+        int max = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            int cnt = 0;
+
+            for(int j=0; j<n; j++)
+            {
+                if(i == j) continue;
+                if(map[i][j] <= 2) cnt++;
+            }
+            max = Math.max(max, cnt);
+        }
+        System.out.print(max);
+    }
+}
