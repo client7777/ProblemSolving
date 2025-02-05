116ad8f9efbca3af856a1b777e80496fad1b5358
client7777 <106692694+client7777@users.noreply.github.com>
2025-02-05 21:03:31 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,12 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) 
     {
-        int ans = 0;
-        
-        if(num1 == num2)
-            ans = 1;
-        else ans = -1;
-        
-        return ans;
+        return (num1 == num2) ? 1 : -1;
     }
 }
