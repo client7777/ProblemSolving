b9850290d8509ac00e7abdd2fdc3b8a944c717e2
client7777 <106692694+client7777@users.noreply.github.com>
2025-02-07 13:55:32 +0900

    Update 카드.java

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,49 +1,37 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args)throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		long[] arr = new long[N];
-		
-		for(int i=0; i<N; i++)
-		{
-			arr[i] = Long.parseLong(br.readLine());
-		}
-		Arrays.sort(arr); // 배열을 입력받아서 오름차순으로 정렬
-		
-		int cnt = 1;
-		long mxval = arr[0];
-		int mxcnt = 0;
-		for(int i=1; i<N; i++)
-		{
-			if(arr[i] == arr[i-1]) cnt++;
-			else 
-			{
-				if(cnt > mxcnt)
-				{
-					mxcnt =cnt;
-					mxval = arr[i-1];
-				}
-				cnt = 1;
-			}
-			if(i == N-1)
-			{
-				if(cnt> mxcnt)
-				{
-					mxcnt = cnt;
-					mxval = arr[i-1];
-				}
-			}
-			
-		}
-		
-		System.out.print(mxval);
-	}
+        int n = Integer.parseInt(br.readLine());
 
+        Map<Long, Integer> map = new HashMap<>(); // 숫자, 등장횟수
+
+        for(int i=0; i<n; i++)
+        {
+            long num = Long.parseLong(br.readLine());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        long maxNum = 0;
+        int maxCnt = 0;
+
+        for(Map.Entry<Long, Integer> entry : map.entrySet())
+        {
+            long key = entry.getKey();
+            int cnt = entry.getValue();
+
+            if(cnt > maxCnt || (cnt == maxCnt && key < maxNum))
+            {
+                maxCnt = cnt;
+                maxNum = key;
+            }
+        }
+
+        System.out.print(maxNum);
+    }
 }
