0e525c8733a3da8e7b834ef17147fc96154edfa9
client7777 <106692694+client7777@users.noreply.github.com>
2025-02-12 23:26:22 +0900

    Update 가장 긴 바이토닉 부분 수열.java

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -5,67 +5,49 @@ import java.util.StringTokenizer;
 
 public class Main
 {
-    static int n;
-    static int[] increase; //증가하는 부분 수열
-    static int[] decrease; //감소하는 부분 수열
-    static int[] arr;
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
 
-        arr = new int[n+1];
-        increase = new int[n+1];
-        decrease = new int[n+1];
+        int n = Integer.parseInt(br.readLine());
+
+        int[] d_left = new int[n+1];
+        int[] d_right = new int[n+1];
+        int[] arr = new int[n+1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=1; i<=n; i++)
         {
             int num = Integer.parseInt(st.nextToken());
             arr[i] = num;
-            increase[i] = 1;
-            decrease[i] = 1;
+            d_left[i] = 1;
+            d_right[i] = 1;
         }
 
-        increaseLength();
-        decreaseLength();
-        
-        int max = 0;
-
+        //왼 -> 오 방향으로 증가하는 부분 수열
         for(int i=1; i<=n; i++)
         {
-            max = Math.max(max, increase[i] + decrease[i]);
+            for(int j=1; j<i; j++)
+            {
+                if(arr[i] > arr[j]) d_left[i] = Math.max(d_left[i], d_left[j] + 1);
+            }
+        }
+
+        //오 -> 왼 방향으로 증가하는 부분 수열
+        for(int i=n; i>0; i--)
+        {
+            for(int j=n; j>i; j--)
+            {
+                if(arr[i] > arr[j]) d_right[i] = Math.max(d_right[i], d_right[j] + 1);
+            }
+        }
+        
+        int max = 0;
+        for(int i=1; i<=n; i++)
+        {
+            max = Math.max(max, d_left[i] + d_right[i]);
         }
 
         System.out.print(max-1);
     }
-
-    static void increaseLength()
-    {
-        for(int i=1; i<=n; i++)
-        {
-            for(int j=1; j<i; j++)
-            {
-                if(arr[i] > arr[j])
-                {
-                    increase[i] = Math.max(increase[i], increase[j] + 1);
-                }
-            }
-        }
-    }
-
-    static void decreaseLength()
-    {
-        for(int i=1; i<=n; i++)
-        {
-            for(int j=1; j<i; j++)
-            {
-                if(arr[i] < arr[j])
-                {
-                    increase[i] = Math.max(increase[i], increase[j] + 1);
-                }
-            }
-        }
-    }
-
 }
