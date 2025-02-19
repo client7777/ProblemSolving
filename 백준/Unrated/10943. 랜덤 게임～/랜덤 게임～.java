a97be60a3e6117ae922d1ffa1596782186734af1
client7777 <106692694+client7777@users.noreply.github.com>
2025-02-19 15:18:26 +0900

    [Unrated] Title: 랜덤 게임~, Time: 92 ms, Memory: 13840 KB -BaekjoonHub

---
+++ 백준/Unrated/10943. 랜덤 게임～/랜덤 게임～.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.util.Random;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        System.out.print(7);
+    }
+}
