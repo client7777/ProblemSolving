d5abcf63f510acd311d4cf625aa123b89c8aac9e
client7777 <106692694+client7777@users.noreply.github.com>
2025-02-24 18:44:38 +0900

    [Gold IV] Title: 텔레포트, Time: 3104 ms, Memory: 19272 KB -BaekjoonHub

--- 백준/Gold/16958. 텔레포트/텔레포트.java
+++ 백준/Gold/16958. 텔레포트/텔레포트.java
@@ -24,24 +24,27 @@ public class Main
             pos[i] = new Pos(s,x,y);
         }
 
-        int[][] dist = new int[1001][1001];
+        int[][] dist = new int[n+1][n+1];
         for(int i=1; i<=n; i++)
         {
             for(int j=1; j<=n; j++)
             {
                 if(i == j) continue;
 
+                //각 노드 사이의 거리를 맨해튼 거리로 초기화
                 int gap = Math.abs(pos[i].x - pos[j].x) + Math.abs(pos[i].y - pos[j].y);
 
                 dist[i][j] = gap;
 
+                //i번 노드, j번 노드가 특별한 도시이면 텔레포트 거리와 맨해튼 거리 중 최단 거리를 택함
                 if(pos[i].s == 1 && pos[j].s == 1)
                 {
                     dist[i][j] = Math.min(gap, t);
                 }
             }
         }
-
+        
+        //플로이드 알로리즘
         for(int k=1; k<=n; k++)
         {
             for(int i=1; i<=n; i++)
@@ -59,6 +62,7 @@ public class Main
         int m = Integer.parseInt(br.readLine());
         for(int i=0; i<m; i++)
         {
+            //쿼리
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
