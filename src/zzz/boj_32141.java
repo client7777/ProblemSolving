ed5c4bd40ad558c1adb9579272da5e2198a5edf9
client7777 <106692694+client7777@users.noreply.github.com>
2025-02-27 11:01:20 +0900

    ProblemSolving

---
+++ src/zzz/boj_32141.java
@@ -0,0 +1,36 @@
+package zzz;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_32141
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++)
+        {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int cnt = 0;
+        for(int i=0; i<n; i++)
+        {
+            h -= arr[i];
+            cnt++;
+            if(h <= 0) break;
+        }
+
+        System.out.print(h > 0 ? -1 : cnt);
+
+    }
+}
