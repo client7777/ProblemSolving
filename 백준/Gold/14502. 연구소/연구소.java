4dad1e2e1d0a2247f5bfac46adc1b6dde833a873
client7777 <106692694+client7777@users.noreply.github.com>
2025-03-01 03:09:17 +0900

    [Gold IV] Title: 연구소, Time: 312 ms, Memory: 135460 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -0,0 +1,111 @@
+//연구소
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    static int n,m;
+    static int[][] map;
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,1,0,-1};
+    static int safeArea = Integer.MIN_VALUE;
+    static ArrayList<int[]> wall = new ArrayList<>();
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+
+        for(int i=0; i<n; i++)
+        {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++)
+            {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 0)
+                {
+                    //빈칸이라면 벽을 세울 수 있는 칸이므로 리스트에 좌표를 추가
+                    wall.add(new int[]{i,j});
+                }
+            }
+        }
+        funcwall(0,0);
+        System.out.print(safeArea);
+
+    }
+    //벽을 세운다. -> 바이러스를 뿌린다. -> 안전영역의 개수를 구한다.
+    static void funcwall(int w,int at)
+    {
+        if(w == 3)
+        {
+            // 벽을 모두 세운 경우 바이러스를 뿌리러 감
+            virus();
+            return;
+        }
+        for(int i=at; i<wall.size(); i++)
+        {
+            int[] cur = wall.get(i);
+            int x = cur[0];
+            int y = cur[1];
+            //벽을 세우고, 재귀호출, 백트래킹
+            map[x][y] = 1;
+            funcwall(w + 1, i + 1);
+            map[x][y] = 0;
+        }
+    }
+    static void virus()
+    {
+        Queue<int[]> q = new LinkedList<>();
+        int[][] copyMap = new int[n][m];
+        //배열을 복사
+        for(int i=0; i<n; i++)
+        {
+            System.arraycopy(map[i],0,copyMap[i],0,m);
+        }
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(map[i][j] == 2)
+                {
+                    q.add(new int[]{i,j});
+                }
+            }
+        }
+        while (!q.isEmpty())
+        {
+            int[] cur = q.poll();
+            int curX = cur[0];
+            int curY = cur[1];
+            for(int dir=0; dir<4; dir++)
+            {
+                int nX = curX + dx[dir];
+                int nY = curY + dy[dir];
+                if(OOB(nX,nY) || copyMap[nX][nY] != 0) continue;
+                q.add(new int[]{nX,nY});
+                copyMap[nX][nY] = 2;
+            }
+        }
+        //바이러스를 모두 뿌렸으면 안전영역의 개수를 구하러 감
+        countArea(copyMap);
+    }
+    static void countArea(int[][] copyMap)
+    {
+        int safe = 0;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(copyMap[i][j] == 0) safe++;
+            }
+        }
+        safeArea = Math.max(safeArea, safe);
+    }
+    static boolean OOB(int x,int y)
+    {
+        return x < 0 || y < 0 || x >= n || y >= m;
+    }
+}
