feae0da423e75967d866125ab86cd175cada33f9
client7777 <106692694+client7777@users.noreply.github.com>
2025-02-28 10:46:28 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] val = new int[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            val[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(val);
+
+        int ans = 0;
+
+        for(int i=n-1; i>=0; i--)
+        {
+            ans += k/val[i];
+            k %= val[i];
+        }
+        System.out.println(ans);
+    }
+}
