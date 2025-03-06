c165c4e20c50e3a8b7fe99912555a29d69a74306
client7777 <106692694+client7777@users.noreply.github.com>
2025-03-06 20:17:19 +0900

    [Silver IV] Title: 알고리즘 수업 - 피보나치 수 2, Time: 812 ms, Memory: 15984 KB -BaekjoonHub

---
+++ 백준/Silver/24417. 알고리즘 수업 － 피보나치 수 2/알고리즘 수업 － 피보나치 수 2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main
+{
+    static final int MOD = 1_000_000_007;
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long cnt1 = fib(n) % MOD;
+        long cnt2 = (n-2) %MOD;
+
+        System.out.print(cnt1 + " " + cnt2);
+    }
+    
+    static long fib(int n)
+    {
+        if(n == 1 || n == 2) return 1;
+
+        long a = 1, b = 1, c = 0;
+        for(int i=3; i<=n; i++)
+        {
+            c = (a + b) % MOD;
+            a = b;
+            b = c;
+        }
+
+        return c;
+    }
+}
