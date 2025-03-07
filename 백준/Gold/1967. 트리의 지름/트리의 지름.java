90a8fd11d395b43611d4f75f79f406837133e91c
client7777 <106692694+client7777@users.noreply.github.com>
2025-03-07 16:55:46 +0900

    [Gold IV] Title: 트리의 지름, Time: 188 ms, Memory: 23076 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,63 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main
+public class Main 
 {
-    static int max = 0;
-    static boolean[] visit;
+    static int n, maxDist, farthestNode;
     static ArrayList<int[]>[] graph;
-    public static void main(String[] args) throws IOException
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException 
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        graph = new ArrayList[n+1];
-        for(int i=1; i<=n; i++)
-        {
-            graph[i] = new ArrayList<>();
+        if (n == 1) { // 노드가 하나인 경우, 지름은 0
+            System.out.print(0);
+            return;
         }
 
+        graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+
         StringTokenizer st;
-        for(int i=0; i<n-1; i++)
-        {
+        for (int i = 0; i < n - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-
-            graph[u].add(new int[]{v,cost});
-            graph[v].add(new int[]{u,cost});
+            graph[u].add(new int[]{v, cost});
+            graph[v].add(new int[]{u, cost});
         }
 
-        visit = new boolean[n+1];
-        for(int i=1; i<=n; i++)
-        {
-            Arrays.fill(visit, false);
-            dfs(i, 0);
-        }
+        
+        visit = new boolean[n + 1];
+        dfs(1, 0);
 
-        System.out.print(max);
+        
+        visit = new boolean[n + 1];
+        maxDist = 0;
+        dfs(farthestNode, 0);
+
+        System.out.print(maxDist);
     }
 
-    static void dfs(int node, int sum)
-    {
+    static void dfs(int node, int sum) {
         visit[node] = true;
 
-        max = Math.max(max, sum);
+        if (sum > maxDist) {
+            maxDist = sum;
+            farthestNode = node;
+        }
 
-        for(int[] next:graph[node])
-        {
-            int nextNode = next[0];
-            int nextCost = next[1];
-
-            if(!visit[nextNode])
-            {
-                dfs(nextNode, sum + nextCost);
-            }
+        for (int[] next : graph[node]) {
+            int nextNode = next[0], nextCost = next[1];
+            if (!visit[nextNode]) dfs(nextNode, sum + nextCost);
         }
     }
 }
