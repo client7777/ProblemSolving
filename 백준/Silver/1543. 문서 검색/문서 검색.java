d1beaf375b9ad242d32444a4c5b7c21becb6f240
client7777 <106692694+client7777@users.noreply.github.com>
2025-03-07 15:35:50 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 14456 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String search = br.readLine();
+
+        str = str.replaceAll(search, "1");
+        
+        int answer = 0;
+
+        for(int i=0; i<str.length(); i++)
+        {
+            if(str.charAt(i) == '1') answer++;
+        }
+
+        System.out.print(answer);
+
+    }
+}
