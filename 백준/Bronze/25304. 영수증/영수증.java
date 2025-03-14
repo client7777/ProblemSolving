af8756f4cbd019451e741b27eeb57aab5e1091c0
client7777 <106692694+client7777@users.noreply.github.com>
2025-03-14 10:25:23 +0900

    [Bronze IV] Title: 영수증, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tot = Integer.parseInt(br.readLine());
+
+        int cnt = Integer.parseInt(br.readLine());
+
+        int amount = 0;
+        StringTokenizer st;
+        while (cnt -- > 0)
+        {
+            st = new StringTokenizer(br.readLine());
+            int price = Integer.parseInt(st.nextToken());
+            int ea = Integer.parseInt(st.nextToken());
+
+            amount += (price * ea);
+        }
+
+        System.out.print(tot == amount ? "Yes" : "No");
+    }
+}
