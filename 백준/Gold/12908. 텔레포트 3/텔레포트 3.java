5933e1d9db8e4743c26afb21291d87a50c7f3f12
client7777 <106692694+client7777@users.noreply.github.com>
2025-03-15 23:13:02 +0900

    [Gold V] Title: 텔레포트 3, Time: 108 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Gold/12908. 텔레포트 3/텔레포트 3.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int startX = Integer.parseInt(st.nextToken());
+        int startY = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int endX = Integer.parseInt(st.nextToken());
+        int endY = Integer.parseInt(st.nextToken());
+
+        Node[] nodes = new Node[8];
+        long[][] dist = new long[8][8];
+
+        for(int i=0; i<8; i++)
+        {
+            Arrays.fill(dist[i], Long.MAX_VALUE / 2);
+            dist[i][i] = 0;
+        }
+
+        nodes[0] = new Node(startX, startY);
+        nodes[7] = new Node(endX, endY);
+
+        //맨해튼 거리
+        dist[0][7] = dist[7][0] = Math.abs(endX - startX) + Math.abs(endY - startY);
+
+        for(int i=1; i<=6; i+=2)
+        {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            nodes[i] = new Node(x1, y1);
+
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            nodes[i+1] = new Node(x2,y2);
+
+            dist[i][i+1] = dist[i+1][i] = Math.min((Math.abs(x2-x1) + Math.abs(y2-y1)), 10);
+        }
+
+        for(int i=0; i<8; i++)
+        {
+            for(int j=0; j<8; j++)
+            {
+                if(i == j) continue;
+                dist[i][j] = Math.min(dist[i][j], (Math.abs(nodes[i].x - nodes[j].x) + Math.abs(nodes[i].y - nodes[j].y)));
+            }
+        }
+        for(int k=0; k<8; k++)
+        {
+            for(int i=0; i<8; i++)
+            {
+                for(int j=0; j<8; j++)
+                {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        System.out.print(dist[0][7]);
+    }
+
+    static class Node
+    {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
