f24a111582bc7a11618cfdeb557870caaffbd558
client7777 <106692694+client7777@users.noreply.github.com>
2025-03-16 18:43:23 +0900

    [Gold III] Title: 연예인은 힘들어, Time: 132 ms, Memory: 15144 KB -BaekjoonHub

--- 백준/Gold/17270. 연예인은 힘들어/연예인은 힘들어.java
+++ 백준/Gold/17270. 연예인은 힘들어/연예인은 힘들어.java
@@ -2,26 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main
 {
-    static int INF = 1_000_000_000;
+    static int v;
+    static final int INF = 1_000_000_000;
+    static ArrayList<int[]>[] graph;
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int v = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[v+1][v+1];
+        graph = new ArrayList[v+1];
         for(int i=1; i<=v; i++)
         {
-            for(int j=1; j<=v; j++)
-            {
-                if(i == j) continue;
-                map[i][j] = INF;
-            }
+            graph[i] = new ArrayList<>();
         }
 
         for(int i=0; i<m; i++)
@@ -31,57 +31,96 @@ public class Main
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            map[a][b] = Math.min(map[a][b], c);
-            map[b][a] = Math.min(map[b][a], c);
+            //양방향 그래프
+            graph[a].add(new int[]{b,c});
+            graph[b].add(new int[]{a,c});
         }
 
         st = new StringTokenizer(br.readLine());
-        int ji = Integer.parseInt(st.nextToken());
-        int sung = Integer.parseInt(st.nextToken());
+        int j = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
 
-        for(int k=1; k<=v; k++)
-        {
-            for(int i=1; i<=v; i++)
-            {
-                for(int j=1; j<=v; j++)
-                {
-                    if(map[i][j] > map[i][k] + map[k][j])
-                    {
-                        map[i][j] = map[i][k] + map[k][j];
-                    }
-                }
-            }
-        }
+        int[] startFromJ = dijkstra(j);
+        int[] startFromS = dijkstra(s);
 
         int minDist = INF;
         for(int i=1; i<=v; i++)
         {
-            if(i == ji || i == sung) continue;
-            minDist = Math.min(minDist, map[ji][i] + map[sung][i]);
+            if(i == j || i == s) continue;
+            minDist = Math.min(minDist, startFromJ[i] + startFromS[i]);
         }
 
         ArrayList<Integer> candidateNode = new ArrayList<>();
         for(int i=1; i<=v; i++)
         {
-            if(i == ji || i == sung) continue;
-            if(map[ji][i] + map[sung][i] == minDist)
+            if(i == j || i == s) continue;
+            if(startFromJ[i] + startFromS[i] == minDist)
             {
-                if(map[ji][i] <= map[sung][i]) candidateNode.add(i);
+                if(startFromJ[i] <= startFromS[i]) candidateNode.add(i);
             }
         }
 
-        int dist = INF;
         int ans = -1;
-
+        int dist = INF;
         for(int index:candidateNode)
         {
-            if(dist > map[ji][index])
+            if(dist > startFromJ[index])
             {
                 ans = index;
-                dist = map[ji][index];
+                dist = startFromJ[index];
             }
         }
 
         System.out.print(ans);
+    }
+
+    static int[] dijkstra(int start)
+    {
+        int[] dist = new int[v+1];
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(start, 0));
+
+        while (!pq.isEmpty())
+        {
+            Node cur = pq.poll();
+            int curNoe = cur.node;
+            int curDist = cur.dist;
+
+            if(curDist > dist[curNoe]) continue;
+
+            for(int[] next:graph[curNoe])
+            {
+                int nextNode = next[0];
+                int nextDist = next[1];
+
+                if(dist[nextNode] > dist[curNoe] + nextDist)
+                {
+                    dist[nextNode] = dist[curNoe] + nextDist;
+                    pq.add(new Node(nextNode, dist[nextNode]));
+                }
+            }
+
+        }
+        return dist;
+    }
+
+    static class Node implements Comparable<Node>
+    {
+        int node;
+        int dist;
+
+        public Node(int node, int dist)
+        {
+            this.node = node;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.dist, o.dist);
+        }
     }
 }
