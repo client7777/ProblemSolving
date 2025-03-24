32b17c4763a41c1e665f57ab501faca991c7bd79
client7777 <106692694+client7777@users.noreply.github.com>
2025-03-24 15:54:16 +0900

    [Bronze I] Title: SWAPC, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/32158. SWAPC/SWAPC.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        char[] ch = br.readLine().toCharArray();
+
+        ArrayList<Integer> pIndex = new ArrayList<>();
+        ArrayList<Integer> cIndex = new ArrayList<>();
+
+        for(int i=0; i<n; i++)
+        {
+            if(ch[i] == 'P') pIndex.add(i);
+            if(ch[i] == 'C') cIndex.add(i);
+        }
+
+        int l = Math.min(pIndex.size(), cIndex.size());
+
+        for(int i=0; i<l; i++)
+        {
+            ch[pIndex.get(i)] = 'C';
+            ch[cIndex.get(i)] = 'P';
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(char c : ch)
+        {
+            sb.append(c);
+        }
+
+        System.out.print(sb);
+    }
+}
