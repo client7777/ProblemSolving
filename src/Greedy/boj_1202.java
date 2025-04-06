668ebedef3a64ab062bf2b80b5e442e8794ac90f
client7777 <106692694+client7777@users.noreply.github.com>
2025-04-06 14:31:46 +0900

    ProblemSolving

--- src/Greedy/boj_1202.java
+++ src/Greedy/boj_1202.java
@@ -9,43 +9,63 @@ public class boj_1202
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int n = Integer.parseInt(st.nextToken()); // 보석의 개수
-        int k = Integer.parseInt(st.nextToken()); // 가방의 개수
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        ArrayList<int[]> list = new ArrayList<>();
+        Info[] infos = new Info[n];
         for(int i=0; i<n; i++)
         {
             st = new StringTokenizer(br.readLine());
             int m = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            list.add(new int[]{m,v});
+
+            infos[i] = new Info(m,v);
         }
-        list.sort(Comparator.comparingInt(o -> o[0])); // 보석의 무게를 기준으로 오름차순 정렬
+
+        Arrays.sort(infos, (a,b) ->
+        {
+            if(a.m == b.m) return Integer.compare(b.v, a.v);
+            return Integer.compare(a.m, b.m);
+        });
+
         int[] bag = new int[k];
         for(int i=0; i<k; i++)
         {
             bag[i] = Integer.parseInt(br.readLine());
         }
+
         Arrays.sort(bag);
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        long sum = 0;
-        int idx = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(o->-o));
+        long ans = 0;
 
-        for(int b:bag)
+        int idx = 0;
+        for(int weight : bag)
         {
-            while (idx < n && list.get(idx)[0] <= b)
+            while (idx < n && infos[idx].m <= weight)
             {
-                pq.add(list.get(idx)[1]);
+                pq.add(infos[idx].v);
                 idx++;
             }
+
             if(!pq.isEmpty())
             {
-                sum += pq.poll();
+                ans += pq.poll();
             }
         }
-        System.out.print(sum);
+
+        System.out.print(ans);
+    }
+
+    static class Info
+    {
+        int m;
+        int v;
+
+        public Info(int m, int v)
+        {
+            this.m = m;
+            this.v = v;
+        }
     }
 }
-//가격이 높은 보석부터 확인하며 보석을 담을 수 있는 가방중 무게가 가장 작은 가방을 이용해 보석을 담는게 이득이다.
