13a7af8588bc2e86048df0ec60379a19e3fe73be
client7777 <106692694+client7777@users.noreply.github.com>
2025-04-08 16:00:11 +0900

    [Gold IV] Title: 회사 문화 1, Time: 576 ms, Memory: 82680 KB -BaekjoonHub

---
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main
+{
+    static ArrayList<Integer>[] graph;
+    static int[] cost;
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        cost = new int[n+1];
+
+        graph = new ArrayList[n+1];
+        for(int i=1; i<=n; i++)
+        {
+            graph[i] = new ArrayList<>();
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=n; i++)
+        {
+            int p = Integer.parseInt(st.nextToken());
+
+            if(p == -1) continue;
+
+            graph[p].add(i);
+        }
+
+        for(int i=0; i<m; i++)
+        {
+            st = new StringTokenizer(br.readLine());
+            int node = Integer.parseInt(st.nextToken());
+            int val = Integer.parseInt(st.nextToken());
+
+            cost[node] += val;
+        }
+
+        dfs(1);
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=n; i++)
+        {
+            sb.append(cost[i]).append(" ");
+        }
+
+        System.out.print(sb);
+    }
+
+    static void dfs(int node)
+    {
+        for(int next : graph[node])
+        {
+            cost[next] += cost[node];
+            dfs(next);
+        }
+    }
+}
