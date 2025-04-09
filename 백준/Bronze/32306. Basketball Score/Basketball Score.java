a27a1e869df1e2fe1c9970492a2bf56f2866e529
client7777 <106692694+client7777@users.noreply.github.com>
2025-04-09 14:24:46 +0900

    [Bronze IV] Title: Basketball Score, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/32306. Basketball Score/Basketball Score.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        int sumA = 0, sumB = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] strA = br.readLine().split(" ");
+        String[] strB = br.readLine().split(" ");
+
+       for(int i=0; i<3; i++)
+       {
+           sumA += Integer.parseInt(strA[i]) * (i+1);
+       }
+
+        for(int i=0; i<3; i++)
+        {
+            sumB += Integer.parseInt(strB[i]) * (i+1);
+        }
+
+        if(sumA == sumB)
+        {
+            System.out.print(0);
+        }
+        else if(sumA > sumB)
+        {
+            System.out.print(1);
+        }
+        else
+            System.out.print(2);
+    }
+}
