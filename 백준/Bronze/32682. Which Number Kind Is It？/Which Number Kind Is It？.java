28ca9b4f591b26b34b56b37906e27188608511ee
client7777 <106692694+client7777@users.noreply.github.com>
2025-04-18 22:48:08 +0900

    [Bronze III] Title: Which Number Kind Is It?, Time: 248 ms, Memory: 23636 KB -BaekjoonHub

--- 백준/Bronze/32682. Which Number Kind Is It？/Which Number Kind Is It？.java
+++ 백준/Bronze/32682. Which Number Kind Is It？/Which Number Kind Is It？.java
@@ -6,33 +6,34 @@ public class Main
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        int test_case = Integer.parseInt(br.readLine());
 
         boolean[] isSquare = new boolean[1_000_001];
-
+        
         for (int i = 0; i * i <= 1_000_000; i++)
         {
             isSquare[i * i] = true;
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < T; i++)
+
+        while (test_case -- > 0)
         {
-            int N = Integer.parseInt(br.readLine());
-            if (isSquare[N] && N % 2 == 1)
+            int num = Integer.parseInt(br.readLine());
+            if(isSquare[num] && num % 2 == 1)
             {
-                sb.append("OS\n");
-            } else if (isSquare[N])
+                sb.append("OS").append('\n');
+            }
+            else if(num % 2 == 1)
             {
-                sb.append("S\n");
-            } else if (N % 2 == 1)
+                sb.append("O").append('\n');
+            }
+            else if(isSquare[num])
             {
-                sb.append("O\n");
+                sb.append("S").append('\n');
             }
             else
-            {
-                sb.append("EMPTY\n");
-            }
+                sb.append("EMPTY").append('\n');
         }
 
         System.out.print(sb);
