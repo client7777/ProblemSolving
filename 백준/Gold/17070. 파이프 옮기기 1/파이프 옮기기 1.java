30628330026f1a99f8ab167ba80e70e4381d9a6c
client7777 <106692694+client7777@users.noreply.github.com>
2025-04-21 11:20:10 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 188 ms, Memory: 17352 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -23,69 +23,40 @@ public class Main
             }
         }
 
-        bfs();
+        dfs(0,1,1);
         System.out.print(answer);
     }
 
-    static void bfs()
+    static void dfs(int x,int y,int dir)
     {
-        Queue<Node> q = new LinkedList<>();
-        //1 -> 가로 2 -> 세로 3 -> 대각
-        q.add(new Node(0,1,1));
-        
-        while (!q.isEmpty())
+        if(x == n-1 && y == n-1)
         {
-            Node cur = q.poll();
-            int curX = cur.x;
-            int curY = cur.y;
-            int curDir = cur.dir;
-            
-            if(curX == n-1 && curY == n-1)
-            {
-                answer++;
-                continue;
-            }
-            
-            //현재 방향이 가로이거나 대각이면 가로로 이동 가능
-            if(curDir == 1 || curDir == 3)
-            {
-                if(curY + 1 < n && map[curX][curY + 1] == 0)
-                {
-                    q.add(new Node(curX, curY + 1, 1));
-                }
-            }
+            answer++;
+            return;
+        }
 
-            //현재 방향이 세로이거나 대각이면 세로로 이동 가능
-            if(curDir == 2 || curDir == 3)
+        if(dir == 1 || dir == 3)
+        {
+            if(y + 1 < n && map[x][y+1] == 0)
             {
-                if(curX + 1 < n && map[curX + 1][curY] == 0)
-                {
-                    q.add(new Node(curX + 1, curY, 2));
-                }
-            }
-
-            //대각으로 이동은 현재 방향에 상관없이 가능
-            if(curX + 1 < n && curY + 1 < n)
-            {
-                if(map[curX + 1][curY + 1] == 0 && map[curX + 1][curY] == 0 && map[curX][curY + 1] == 0)
-                {
-                    q.add(new Node(curX + 1, curY + 1, 3));
-                }
+                dfs(x,y+1,1);
             }
         }
-    }
 
-    static class Node
-    {
-        int x;
-        int y;
-        int dir;
-
-        public Node(int x, int y, int dir)
+        if(dir == 2 || dir == 3)
         {
-            this.x = x;
-            this.y = y;
-            this.dir = dir;
+            if(x + 1 < n && map[x+1][y] == 0)
+            {
+                dfs(x+1, y, 2);
+            }
+        }
+
+        if(x + 1 < n && y + 1 < n)
+        {
+            if(map[x+1][y+1] == 0 && map[x+1][y] == 0 && map[x][y+1] == 0)
+            {
+                dfs(x+1, y+1, 3);
+            }
         }
     }
 }
