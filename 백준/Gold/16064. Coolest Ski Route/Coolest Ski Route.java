0d3da5fa9b580b2434d206ec821a8b42c1dc6afe
client7777 <106692694+client7777@users.noreply.github.com>
2025-04-29 06:52:36 +0900

    [Gold III] Title: Coolest Ski Route, Time: 156 ms, Memory: 16368 KB -BaekjoonHub

--- 백준/Gold/16064. Coolest Ski Route/Coolest Ski Route.java
+++ 백준/Gold/16064. Coolest Ski Route/Coolest Ski Route.java
@@ -3,23 +3,24 @@ import java.util.*;
 
 public class Main
 {
-    static final int INF = -987654321;
+    static int n,m;
+    static int[] d; // d[i] = i번 노드까지 도달했을 때 얻을 수 있는 최대 점수
+    static int[] inDegree; // 진입차수
+    static ArrayList<Node>[] graph;
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[n+1][n+1];
+        d = new int[n+1];
+        inDegree = new int[n+1];
 
+        graph = new ArrayList[n+1];
         for(int i=1; i<=n; i++)
         {
-            for(int j=1; j<=n; j++)
-            {
-                if(i == j) continue;
-                map[i][j] = INF;
-            }
+            graph[i] = new ArrayList<>();
         }
 
         for(int i=0; i<m; i++)
@@ -29,29 +30,59 @@ public class Main
             int t = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            map[s][t] = Math.max(map[s][t], c);
+            graph[s].add(new Node(t,c));
+            inDegree[t]++;
         }
 
-        for(int k=1; k<=n; k++)
-        {
-            for(int i=1; i<=n; i++)
-            {
-                for(int j=1; j<=n; j++)
-                {
-                    map[i][j] = Math.max(map[i][j], map[i][k] + map[k][j]);
-                }
-            }
-        }
+        topological();
 
         int answer = 0;
         for(int i=1; i<=n; i++)
         {
-            for(int j=1; j<=n; j++)
-            {
-                answer = Math.max(answer, map[i][j]);
-            }
+            answer = Math.max(answer, d[i]);
         }
 
         System.out.print(answer);
+    }
+
+    static void topological()
+    {
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=1; i<=n; i++)
+        {
+            if(inDegree[i] == 0)
+            {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty())
+        {
+            int cur = q.poll();
+
+            for(Node next : graph[cur])
+            {
+                int nextNode = next.node;
+                int nextDist = next.dist;
+
+                d[nextNode] = Math.max(d[nextNode], d[cur] + nextDist);
+
+                if(--inDegree[nextNode] == 0)
+                    q.add(nextNode);
+            }
+        }
+    }
+
+    static class Node
+    {
+        int node;
+        int dist;
+
+        public Node(int node, int dist)
+        {
+            this.node = node;
+            this.dist = dist;
+        }
     }
 }
