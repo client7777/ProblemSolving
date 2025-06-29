c52f6b7b2a5fc36a2591e5b2633fb7361fc678ab
client7777 <106692694+client7777@users.noreply.github.com>
2025-06-30 00:18:51 +0900

    [Bronze III] Title: Cakes, Time: 96 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/21212. Cakes/Cakes.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int min = Integer.MAX_VALUE;
+		while (n-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			min = Math.min(min, b/a);
+		}
+
+		System.out.print(min);
+	}
+}
