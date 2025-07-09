1eb688b9804a54996f1f9d1ebc4456b5c1f81ee8
client7777 <106692694+client7777@users.noreply.github.com>
2025-07-09 16:56:31 +0900

    [Silver II] Title: 숫자판 점프, Time: 124 ms, Memory: 15500 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
@@ -6,7 +6,6 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int answer = 0;
 	static int[] dx = {-1, 0, 1, 0};
 	static int[] dy = {0, 1, 0, -1};
 	static Set<String> set = new HashSet<>();
