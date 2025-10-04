f6fc101c3ead76990d1ac86ee9dbf0168a41e3fe
client7777 <106692694+client7777@users.noreply.github.com>
2025-10-04 22:01:25 +0900

    [Gold III] Title: 인하니카 공화국, Time: 308 ms, Memory: 37500 KB -BaekjoonHub

---
+++ 백준/Gold/12784. 인하니카 공화국/인하니카 공화국.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	static ArrayList<Node>[] graph;
+	static boolean[] visit;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test_case = Integer.parseInt(br.readLine());
+
+		while (test_case-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+
+			graph = new ArrayList[n+1];
+			for(int i = 1; i <= n; i++){
+				graph[i] = new ArrayList<>();
+			}
+
+			for(int i = 0; i < m; i++){
+				st = new StringTokenizer(br.readLine());
+				int u = Integer.parseInt(st.nextToken());
+				int v = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+
+				graph[u].add(new Node(v,c));
+				graph[v].add(new Node(u,c));
+			}
+
+			visit = new boolean[n+1];
+			System.out.println(dfs(1, 0));
+		}
+	}
+
+	static int dfs(int node, int edgeCost){
+
+		boolean isLeaf = true;
+
+		visit[node] = true;
+
+		int cost = 0;
+
+		for(Node next : graph[node]){
+			int nextNode = next.node;
+			int nextCost = next.cost;
+
+			if(visit[nextNode]){
+				continue;
+			}
+
+			isLeaf = false;
+
+			cost += Math.min(nextCost, dfs(nextNode, nextCost));
+		}
+
+		if(isLeaf){
+			return edgeCost;
+		}
+
+		return cost;
+	}
+
+	static class Node{
+		int node;
+		int cost;
+
+		public Node(int node, int cost) {
+			this.node = node;
+			this.cost = cost;
+		}
+	}
+}
